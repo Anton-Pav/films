348681170acafd2473c4850d0c1f8d9349eae3b3
Anton <ant.pavlov93@yandex.ru>
2022-09-02 13:43:28 +0300

    Реализовал поиск фильмов

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -8,18 +8,26 @@ class Main extends React.Component {
     state = {
         movies: [],
     }
+
     componentDidMount() {
         fetch('http://www.omdbapi.com/?apikey=ea041e84&s=matrix')
             .then(response => response.json())
             .then(data => this.setState({movies: data.Search}))
     }
 
+    searchMovies = (str) => {
+        fetch(`http://www.omdbapi.com/?apikey=ea041e84&s=${str}`)
+            .then(response => response.json())
+            .then(data => this.setState({movies: data.Search}))
+
+    }
+
     render() {
         const {movies} = this.state
         return <main className='container content'>
-            <Search/>
+            <Search searchMovies={this.searchMovies}/>
             {
-                movies.length ?  (
+                movies.length ? (
                     <Movies movies={this.state.movies}/>
                 ) : <Preloader/>
             }
