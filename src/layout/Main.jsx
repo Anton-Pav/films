9245c5973b788d9a6560b19cebe3a5c40d29b370
Anton <ant.pavlov93@yandex.ru>
2022-09-09 15:15:26 +0300

    перевод классов на функции при помощи хуков

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,42 +1,54 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Movies} from "../components/Movies";
 import {Preloader} from "../components/Preloader";
 import {Search} from "../components/Search";
 
 
-class Main extends React.Component {
-    state = {
-        movies: [],
-        loading: true,
-    }
+const API_KEY = process.env.REACT_APP_API_KEY
+console.log(process.env.REACT_APP_API_KEY)
 
-    componentDidMount() {
-        fetch('http://www.omdbapi.com/?apikey=ea041e84&s=matrix')
+const Main = () => {
+    const [movies, setMovies] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    const searchMovies = (str, type = 'all') => {
+        setLoading(true)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
+            .then((data) => {
+                setLoading(false)
+                setMovies(data.Search)
+            })
+            .catch((err) => {
+                console.log(err)
+                setLoading(false)
+            })
     }
 
-    searchMovies = (str, type = 'all') => {
-        this.setState({loading: true});
-        fetch(`http://www.omdbapi.com/?apikey=ea041e84&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+    useEffect(() => {
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
+            .then((data) => {
+                setMovies(data.Search)
+                setLoading(false)
+            })
+            .catch((err) => {
+                console.log(err)
+                setLoading(false)
+            })
+    }, [])
 
-    }
 
-    render() {
-        const {movies, loading} = this.state
+    return <main className='container content'>
+        <Search searchMovies={searchMovies}/>
+        {loading ? (
+            <Preloader/>
+        ) : (
+            <Movies movies={movies}/>
+        )}
 
-        return <main className='container content'>
-            <Search searchMovies={this.searchMovies}/>
-            {loading ? (
-                    <Preloader/>
-                ) : (
-                    <Movies movies={movies}/>
-            )}
+    </main>
 
-        </main>
-    }
 }
 
 export {Main}
